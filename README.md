e78eea0b8e2b829394d218424cfe91537001b4d5
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2023-09-17 13:24:16 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# acervo_db
+# projeto_db
+
+Atividade de conexão do Banco de Dados (MySQL) com o JavaScript através do Sequelize ORM + nodeJS + Express.
---
+++ config.js
@@ -0,0 +1,11 @@
+//config.js
+module.exports = {
+    development: {
+        username:'root',
+        password:'root',
+        database:'biblioteca_db',
+        host:'localhost',
+        dialect:'mysql'
+    }
+    
+};
